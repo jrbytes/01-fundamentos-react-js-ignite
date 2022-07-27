9d1ecb27d7e95cb727b03d3d330312d5a8259e30
Jr Bytes <itamazor@gmail.com>
2022-07-27 11:24:34 -0300

    initial commit

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import { Avatar } from './Avatar'
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return(
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/jrbytes.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Júnior Bytes</strong>
+              <time title="11 de maio às 08:33h" dateTime='2022-05-11 08:33:00'>Cerca de 1h atrás</time>
+            </div>
+
+            <button title='Deletar comentário'>
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
